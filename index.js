f168df8ce17219ec1f4bb2cad8eba218edaa9bb3
Zachary <zacharylampi96@gmail.com>
2022-06-06 20:53:24 -0400

    commit tree d7ba2e8ab401bd75111062ad7b60f567ddf8d473

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const saturdayFun = function(act= "roller-skate") {
+    return `This Saturday, I want to ${act}!`
+}
+
+const mondayWork = function(text = "go to the office") {
+    return `This Monday, I will ${text}.`
+}
+
+function wrapAdjective(symb) {
+  const innerFunction = function (adj) {
+    return `You are ${symb}${adj}${symb}!`;
+  };
+  return innerFunction;
+}
